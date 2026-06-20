7cd61df0078df82380d5b8bab577edde6e8c9f0d
frztmr <frztmr.webdev@gmail.com>
2026-06-20 16:51:29 +0700

    organicly added product data. just like another data, i added it organicly into an array. organic means no AI.

---
+++ data/product.ts
@@ -0,0 +1,42 @@
+export const carouselData: {
+    prod_name: string,
+    image: string
+}[] = [
+        {
+            prod_name: 'Speaker 899 Q',
+            image: '/product_list/01.png',
+        },
+        {
+            prod_name: 'Speaker 899 V',
+            image: '/product_list/02.png',
+        },
+        {
+            prod_name: 'Speaker 883 B',
+            image: '/product_list/03.png',
+        },
+        {
+            prod_name: 'Speaker 896 Q',
+            image: '/product_list/04.png',
+        },
+        {
+            prod_name: 'Speaker 899 K',
+            image: '/product_list/05.png',
+        },
+        {
+            prod_name: 'Speaker 897 Z',
+            image: '/product_list/06.png',
+        },
+        {
+            prod_name: 'Speaker 881 B',
+            image: '/product_list/07.png',
+        },
+        {
+            prod_name: 'Speaker 881 H',
+            image: '/product_list/08.png',
+        },
+        {
+            prod_name: 'Speaker 898 R',
+            image: '/product_list/09.png',
+        },
+
+    ]
